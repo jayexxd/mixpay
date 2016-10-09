b789bb1e51b1f905b9fe7bc03a417bc3052aedf7
jia <xdjiaxd@gmail.com>
2016-10-09 14:17:03 +0800

    pls work

--- mixpay/settings.py
+++ mixpay/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+from os import environ
 GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
