a0695d9740d6cc54bad0107edb94edf00f2fc7af
jia <xdjiaxd@gmail.com>
2016-10-09 14:37:41 +0800

    static

--- mixpay/settings.py
+++ mixpay/settings.py
@@ -127,17 +127,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 try:
     LOCAL_SETTINGS
